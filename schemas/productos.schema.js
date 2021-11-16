e6f46bd63e15a7183bb63676e714482b87f6d5fd
AlextremeGamer <ing.javier.perez@gmail.com>
2021-11-16 01:04:31 -0500

    Update productos.schema.js

--- schemas/productos.schema.js
+++ schemas/productos.schema.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
-const { Schema } = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const esquema = new Schema({
-  nombre: { type: String, required: true},
-  color: { type: String, required: true},
-  tipo_mascota: { type: String, required: true},
-  raza: { type: String, required: true},
-  precio: { type: Number, required: true},
-  oferta: { type: Boolean, required: true},
-  tamanio: { type: String, required: true}
-})
-module.exports = mongoose.model('Producto', esquema)
+  nombre: { type: String, required: true },
+  color: { type: String, required: true },
+  tipo_mascota: { type: String, required: true },
+  raza: { type: String, required: true },
+  precio: { type: Number, required: true },
+  oferta: { type: Boolean, required: true },
+  tamano: { type: String, required: true }
+});
+module.exports = mongoose.model("Producto", esquema);
