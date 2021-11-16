a70d909d971e3ae545ea9bca7fe97a05104ea95f
AlextremeGamer <ing.javier.perez@gmail.com>
2021-11-16 01:06:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const morgan = require('morgan')
-const mongoose = require('./connection')
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("./connection");
 const app = express();
 
-// configuracion
-app.set('port', process.env.PORT || 3000 )
-app.use(morgan('dev'))
-app.use(express.json())
+//Configuracion
+app.set("port", process.env.PORT || 3000);
+app.use(morgan("dev"));
+app.use(express.json());
 
-// lista de rutas base
-app.use('/api/productos', require('./routes/productos.routes'))
+//Lista de rutas base
+app.use("/api/productos", require("./routes/productos.routes"));
 
-//arranque
-app.listen(app.get('port'), () => {
-  console.log(process.env.npm_package_name + " Iniciando en puerto " + app.get('port'))
+//Arranque
+app.listen(app.get("port"), () => {
+  console.log(
+    process.env.npm_package_name + " iniciado en puerto " + app.get("port")
+  );
 });
