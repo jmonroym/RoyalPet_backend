77e8a20d89287ced68a4eab280d1ca361e41dd2f
wilmer_muñeton <wilmer.munetonv@gmail.com>
2021-11-27 18:52:38 -0500

    se agrega la opcion de actualizacion de usuario

--- operations/registro_usuarios.operations.js
+++ operations/registro_usuarios.operations.js
@@ -6,4 +6,21 @@ const registro = new coleccionRegistro_usuarios(req.body);
 await registro.save();
 res.json({"status":"Registro exitoso"})
 }
+
+operaciones.actualizarUsuario = async function(req, res){
+  const { id } = req.params;
+  const usuario = {
+    nombres: req.body.nombres,
+    apellidos: req.body.apellidos,
+    correo_electronico: req.body.correo_electronico,
+    numero_celular: req.body.numero_celular,
+    fecha_nacimiento: req.body.fecha_nacimiento,
+    cedula: req.body.cedula
+  }
+  console.log(usuario)
+  await coleccionRegistro_usuarios.findByIdAndUpdate(req.params.id, {$set: usuario}, {new:false});
+  res.json({"status":"usuario actualizado"})
+}
+
+
 module.exports = operaciones;
--- routes/registro_usuarios.routes.js
+++ routes/registro_usuarios.routes.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const operacion = require("../operations/registro_usuarios.operations");
 
 router.post("/", operacion.crearUsuario);
+router.put("/:id" , operacion.actualizarUsuario);
 
 module.exports = router;
