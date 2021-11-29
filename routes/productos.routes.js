f4902e1d79df33de2140be5ae7948ddea4841d92
AlextremeGamer <ing.javier.perez@gmail.com>
2021-11-28 19:59:33 -0500

    Update productos.routes.js

--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -5,7 +5,7 @@ router.get("/", operacion.getProductos);
 router.get("/", operacion.getProductosFiltro);
 router.get("/:id", operacion.getProducto);
 router.post("/", operacion.crearProducto);
-router.put("/:id" , operacion.actualizarProducto);
+router.put("/:id", operacion.actualizarProducto);
 router.delete("/:id", operacion.borrarProducto);
 
 module.exports = router;
