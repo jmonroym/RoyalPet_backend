98aa62b729ce16348c5cdb006f14d3f16f42fa8b
AlextremeGamer <ing.javier.perez@gmail.com>
2021-11-28 19:57:53 -0500

    Update registro_usuarios.operations.js
    TO DO: " pienso que el nombre debía ser solo "usuarios.operations.js" dado que registro es ambiguo como sustantivo y como verbo

--- operations/registro_usuarios.operations.js
+++ operations/registro_usuarios.operations.js
@@ -1,20 +1,18 @@
 const coleccionRegistro_usuarios = require("../schemas/registro_usuarios.schema");
 const operaciones = {};
 
-
 operaciones.getUsuario = async function(req, res) {
+  const usuario = await coleccionRegistro_usuarios.find(req.query);
+  res.json(usuario);
+};
 
-		const usuario = await coleccionRegistro_usuarios.find(req.query)
-		res.json(usuario)
-}
+operaciones.crearUsuario = async function(req, res) {
+  const registro = new coleccionRegistro_usuarios(req.body);
+  await registro.save();
+  res.json({ status: "Registro exitoso" });
+};
 
-operaciones.crearUsuario = async function(req, res){
-const registro = new coleccionRegistro_usuarios(req.body);
-await registro.save();
-res.json({"status":"Registro exitoso"})
-}
-
-operaciones.actualizarUsuario = async function(req, res){
+operaciones.actualizarUsuario = async function(req, res) {
   const { id } = req.params;
   const usuario = {
     nombres: req.body.nombres,
@@ -23,11 +21,14 @@ operaciones.actualizarUsuario = async function(req, res){
     numero_celular: req.body.numero_celular,
     fecha_nacimiento: req.body.fecha_nacimiento,
     cedula: req.body.cedula
-  }
-  console.log(usuario)
-  await coleccionRegistro_usuarios.findByIdAndUpdate(req.params.id, {$set: usuario}, {new:false});
-  res.json({"status":"usuario actualizado"})
-}
-
+  };
+  console.log(usuario);
+  await coleccionRegistro_usuarios.findByIdAndUpdate(
+    req.params.id,
+    { $set: usuario },
+    { new: false }
+  );
+  res.json({ status: "usuario actualizado" });
+};
 
 module.exports = operaciones;
