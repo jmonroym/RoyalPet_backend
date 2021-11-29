2e9ca59487a5a611eb7c2b9754b82b7c97497191
AlextremeGamer <ing.javier.perez@gmail.com>
2021-11-28 20:01:42 -0500

    Update registro_usuarios.routes.js
    TO DO: Pienso que el nombre debio ser "usuarios.routes.js"

--- routes/registro_usuarios.routes.js
+++ routes/registro_usuarios.routes.js
@@ -3,6 +3,6 @@ const operacion = require("../operations/registro_usuarios.operations");
 
 router.get("/", operacion.getUsuario);
 router.post("/", operacion.crearUsuario);
-router.put("/:id" , operacion.actualizarUsuario);
+router.put("/:id", operacion.actualizarUsuario);
 
 module.exports = router;
