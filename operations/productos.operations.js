2d068ab86fc65fcfe28cbf1a7d6bc451de653e63
AlextremeGamer <ing.javier.perez@gmail.com>
2021-11-28 19:53:16 -0500

    Update productos.operations.js

--- operations/productos.operations.js
+++ operations/productos.operations.js
@@ -1,28 +1,30 @@
 const coleccionProductos = require("../schemas/productos.schema");
 const operaciones = {};
 
-operaciones.getProducto = async function(req, res){
+operaciones.getProducto = async function(req, res) {
   const dato = await coleccionProductos.findById(req.params.id);
   res.json(dato);
-}
+};
 
 operaciones.getProductos = async function(req, res) {
   const datos = await coleccionProductos.find();
   res.json(datos);
 };
 
-operaciones.getProductosFiltro = async function(req, res){
-  const filtro = await coleccionProductos.find({
-    oferta:false
-  }).exec();
+operaciones.getProductosFiltro = async function(req, res) {
+  const filtro = await coleccionProductos
+    .find({
+      oferta: false
+    })
+    .exec();
   res.json(filtro);
-}
-operaciones.crearProducto = async function(req, res){
-const producto = new coleccionProductos(req.body);
-await producto.save();
-res.json({"status":"producto guardado exitosamente!"})
-}
-operaciones.actualizarProducto = async function(req, res){
+};
+operaciones.crearProducto = async function(req, res) {
+  const producto = new coleccionProductos(req.body);
+  await producto.save();
+  res.json({ status: "producto guardado exitosamente!" });
+};
+operaciones.actualizarProducto = async function(req, res) {
   const { id } = req.params;
   const producto = {
     nombre: req.body.nombre,
@@ -33,14 +35,18 @@ operaciones.actualizarProducto = async function(req, res){
     oferta: req.body.oferta,
     tamanio: req.body.tamanio,
     categoria: req.body.categoria
-  }
-  console.log(producto)
-  await coleccionProductos.findByIdAndUpdate(req.params.id, {$set: producto}, {new:true});
-  res.json({"status":"producto actualizado"})
-}
-operaciones.borrarProducto = async function(req, res){
+  };
+  console.log(producto);
+  await coleccionProductos.findByIdAndUpdate(
+    req.params.id,
+    { $set: producto },
+    { new: true }
+  );
+  res.json({ status: "producto actualizado" });
+};
+operaciones.borrarProducto = async function(req, res) {
   await coleccionProductos.findByIdAndRemove(req.params.id);
-  res.json({"status":"producto borrado"})
-}
+  res.json({ status: "producto borrado" });
+};
 
 module.exports = operaciones;
